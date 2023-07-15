2296ac6f913c2bac5febf575cfd118c4e60c5434
daominhngoc2003 <ngocdm22052003@gmail.com>
2023-07-15 23:50:35 +0700

    [BE] Update model Product

--- src/model/Product.js
+++ src/model/Product.js
@@ -5,20 +5,45 @@ const ProductSchema = new mongoose.Schema(
     product_name: {
       type: String,
       require: true,
+      minlength: 3,
+      maxlength: 50,
     },
     product_price: {
       type: Number,
       min: 0,
     },
+    product_discount: {
+      type: Number,
+      min: 0,
+    },
+    product_size: {
+      type: String,
+    },
+    product_color: {
+      type: String,
+    },
     product_images: {
       type: Object,
       require: true,
     },
-    product_deScription: {
+    product_deScription_sort: {
       type: String,
+      minlength: 6,
+      maxlength: 255,
+    },
+    product_deScription_long: {
+      type: String,
+      minlength: 6,
+      maxlength: 255,
     },
     product_quantity: {
       type: Number,
+      min: 0,
+      minlength: 0,
+      maxlength: 1000,
+    },
+    product_status: {
+      type: Boolean,
     },
     categoryId: {
       type: mongoose.Types.ObjectId,
