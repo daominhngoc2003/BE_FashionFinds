b7d3f633eedd3130418846351a2a62ae8ad39128
quandev99 <nguyenvietquan2605@gmail.com>
2023-08-06 11:59:40 +0700

    success Cart

--- src/model/Cart.js
+++ src/model/Cart.js
@@ -1,43 +1,14 @@
 import mongoose from "mongoose";
 
-const CartSchema = new mongoose.Schema(
-  {
-    userId: {
+const cartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  items: [
+    {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+      ref: "CartItem",
     },
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-    product_name: {
-      type: String,
-      required: true,
-    },
-    product_image: {
-      type: String,
-      required: true,
-    },
-    color: {
-      type: String,
-      required: true,
-    },
-    size: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      default: 1,
-    },
-  },
-  { timestamps: true, versionKey: false }
-);
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
 
-export default mongoose.model("Cart", CartSchema);
+export default mongoose.model("Cart", cartSchema);
