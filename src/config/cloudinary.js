0faab1d03a27c49b494dc64de324a6a34f31da2a
daominhngoc2003 <ngocdm22052003@gmail.com>
2023-08-07 20:03:32 +0700

    [REMOVE] All

--- src/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "djlylbhrb",
-  api_key: "569577189513946",
-  api_secret: "AFKVYRBk-sIRNUr9orot1mMBJ-8",
-});
-
-export default cloudinary;
--- src/routes/Auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login, refreshToken, register } from "../controller/Auth";
-
-const router = express.Router();
-router.post("/signup", register);
-router.post("/signin", login);
-router.post("refresh-token", refreshToken);
-export default router;
